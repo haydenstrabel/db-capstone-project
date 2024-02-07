f4796420ad56fd990607b737ff092adfe4ddfdea
haydenstrabel <124462438+haydenstrabel@users.noreply.github.com>
2024-02-07 08:21:43 -0500

    adding sales report and modifying ER diagram

--- db-capstone-project/LittleLemonDB.sql
+++ db-capstone-project/LittleLemonDB.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema LittleLemonDB
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8mb3 ;
 USE `LittleLemonDB` ;
 
 -- -----------------------------------------------------
@@ -19,10 +19,11 @@ USE `LittleLemonDB` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`CustomerDetails` (
   `CustomerID` INT NOT NULL,
-  `CustomerName` VARCHAR(45) NULL,
-  `ContactDetails` VARCHAR(45) NULL,
+  `CustomerName` VARCHAR(45) NULL DEFAULT NULL,
+  `ContactDetails` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`CustomerID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -30,28 +31,28 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
   `BookingID` INT NOT NULL,
-  `Date` DATE NULL,
-  `TableNumber` INT NULL,
-  `CustomerID` INT NULL,
+  `Date` DATE NULL DEFAULT NULL,
+  `TableNumber` INT NULL DEFAULT NULL,
+  `CustomerID` INT NULL DEFAULT NULL,
   PRIMARY KEY (`BookingID`),
   INDEX `CustomerID_idx` (`CustomerID` ASC) VISIBLE,
   CONSTRAINT `CustomerID`
     FOREIGN KEY (`CustomerID`)
-    REFERENCES `LittleLemonDB`.`CustomerDetails` (`CustomerID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `LittleLemonDB`.`CustomerDetails` (`CustomerID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `LittleLemonDB`.`OrderDeliverStatus`
+-- Table `LittleLemonDB`.`OrderDeliveryStatus`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDeliverStatus` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDeliveryStatus` (
   `DeliveryID` INT NOT NULL,
-  `DeliveryDate` DATE NULL,
-  `Status` VARCHAR(45) NULL,
+  `DeliveryDate` DATE NULL DEFAULT NULL,
+  `Status` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`DeliveryID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -59,25 +60,22 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
   `OrderID` INT NOT NULL,
-  `OrderDate` DATE NULL,
-  `Quantity` INT NULL,
-  `TotalCost` DECIMAL NULL,
-  `DeliveryID` INT NULL,
-  `BookingID` INT NULL,
+  `OrderDate` DATE NULL DEFAULT NULL,
+  `Quantity` INT NULL DEFAULT NULL,
+  `TotalCost` DECIMAL(10,0) NULL DEFAULT NULL,
+  `DeliveryID` INT NULL DEFAULT NULL,
+  `BookingID` INT NULL DEFAULT NULL,
   PRIMARY KEY (`OrderID`),
   INDEX `DeliveryID_idx` (`DeliveryID` ASC) VISIBLE,
   INDEX `BookingID_idx` (`BookingID` ASC) VISIBLE,
-  CONSTRAINT `DeliveryID`
-    FOREIGN KEY (`DeliveryID`)
-    REFERENCES `LittleLemonDB`.`OrderDeliverStatus` (`DeliveryID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `BookingID`
     FOREIGN KEY (`BookingID`)
-    REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`),
+  CONSTRAINT `DeliveryID`
+    FOREIGN KEY (`DeliveryID`)
+    REFERENCES `LittleLemonDB`.`OrderDeliveryStatus` (`DeliveryID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -85,18 +83,48 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menu` (
   `MenuID` INT NOT NULL,
-  `Cuisine` VARCHAR(45) NULL,
-  `CourseType` VARCHAR(45) NULL,
-  `OrderID` INT NULL,
+  `Cuisine` VARCHAR(45) NULL DEFAULT NULL,
+  `OrderID` INT NULL DEFAULT NULL,
   PRIMARY KEY (`MenuID`),
   INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE,
   CONSTRAINT `OrderID`
     FOREIGN KEY (`OrderID`)
-    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`)
+    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`MenuItems`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItems` (
+  `ItemID` INT NOT NULL,
+  `CourseName` VARCHAR(255) NULL,
+  `Cost` INT NULL,
+  `MenuId` INT NULL,
+  `MenuItemscol` VARCHAR(45) NULL,
+  PRIMARY KEY (`ItemID`),
+  INDEX `MenuID_idx` (`MenuId` ASC) VISIBLE,
+  CONSTRAINT `MenuID`
+    FOREIGN KEY (`MenuId`)
+    REFERENCES `LittleLemonDB`.`Menu` (`MenuID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+USE `LittleLemonDB` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `LittleLemonDB`.`ordersview`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`ordersview` (`OrderID` INT, `Quantity` INT, `TotalCost` INT);
+
+-- -----------------------------------------------------
+-- View `LittleLemonDB`.`ordersview`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LittleLemonDB`.`ordersview`;
+USE `LittleLemonDB`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `littlelemondb`.`ordersview` AS select `littlelemondb`.`orders`.`OrderID` AS `OrderID`,`littlelemondb`.`orders`.`Quantity` AS `Quantity`,`littlelemondb`.`orders`.`TotalCost` AS `TotalCost` from `littlelemondb`.`orders` where (`littlelemondb`.`orders`.`Quantity` > 2);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
